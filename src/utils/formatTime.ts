a809e6c82ca250a8f812615bc33bf42950d43486
Dakota Bryant <bryant.dakota@gmail.com>
2021-04-27 00:59:04 -0400

    fixed formatting

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,6 +1,5 @@
 export const formatTime = (timer: number) => {
-  const formattedSeconds = `0${timer}`.slice(-2);
-  const formattedMilliseconds = `0${(timer / 10) % 100}`.slice(-2);
-
-  return `${formattedSeconds} : ${formattedMilliseconds}`;
+  const formattedSeconds = `0${timer}`.slice(0, 2);
+  const formattedMilliseconds = `${(timer / 10) % 100}0`.slice(-2);
+  return `${formattedSeconds} : ${timer === 5 ? '00' : formattedMilliseconds}`;
 };
